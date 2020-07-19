658ad61e3878d5f46c3ffa71b1514442d526b2d5
aldarris <dovs@hwsrv-666133.hostwindsdns.com>
2020-07-19 17:29:00 +0000

    Reverting changes to management.js

--- BondageClub/Screens/Room/Management/Management.js
+++ BondageClub/Screens/Room/Management/Management.js
@@ -5,7 +5,6 @@ var ManagementSub = null;
 var ManagementMistressAngryCount = 0;
 var ManagementMistressReleaseTimer = 0;
 var ManagementPlayerAppearance = null;
-var ManagementPlayerAppearanceBeforeContract = null;
 var ManagementMistressAllowPlay = false;
 var ManagementCanReleaseChastity = true;
 var ManagementEmpty = false;
@@ -273,7 +272,6 @@ function ManagementSendMistressToPrivateRoom(RepChange) {
 function ManagementClubSlaveCollar(RepChange) {
 	ReputationProgress("Dominant", RepChange);
 	CharacterRelease(Player);
-	ManagementPlayerAppearanceBeforeContract = Player.Appearance.slice();
 	InventoryWear(Player, "ClubSlaveCollar", "ItemNeck");
 	LogAdd("ClubSlave", "Management", CurrentTime + 3600000);
 	LogAdd("BlockChange", "Rule", CurrentTime + 3600000);
@@ -284,17 +282,13 @@ function ManagementClubSlaveCollar(RepChange) {
 function ManagementFinishClubSlave(RepChange) {
 	ReputationProgress("Dominant", RepChange);
 	CharacterChangeMoney(Player, 80);
-	if(null != ManagementPlayerAppearanceBeforeContract){
-		Player.Appearance = ManagementPlayerAppearanceBeforeContract;
-	}else{
-		if (Player.IsOwned()) InventoryWear(Player, "SlaveCollar", "ItemNeck");
-		else {
-			InventoryRemove(Player, "ItemNeck");
-			InventoryRemove(Player, "ItemNeckAccessories");
-			InventoryRemove(Player, "ItemNeckRestraints");
-		}
-		if (Player.IsNaked()) CharacterDress(Player, ManagementPlayerAppearance);
+	if (Player.IsOwned()) InventoryWear(Player, "SlaveCollar", "ItemNeck");
+	else {
+		InventoryRemove(Player, "ItemNeck");
+		InventoryRemove(Player, "ItemNeckAccessories");
+		InventoryRemove(Player, "ItemNeckRestraints");
 	}
+	if (Player.IsNaked()) CharacterDress(Player, ManagementPlayerAppearance);
 }
 
 // When the player as club slave gets stopped by a random girl
